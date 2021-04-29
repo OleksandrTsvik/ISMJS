e60c003a38959489a1dc5cb3d44c0ae24e2acfd0
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2021-04-29 23:13:06 +0300

    Added css

--- JS-05/server.js
+++ JS-05/server.js
@@ -6,37 +6,65 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
+const hbs = require("hbs");
 
 app.use(express.static('www'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 let users = {};
 
 io.on('connection', (socket) => {
+    users[socket.id] = 'Anonymous';
+    io.emit('countUsersOnline', Object.keys(users).length);
+    io.emit('user-nick', users[socket.id]);
+    io.emit('listUsers', users);
+
     socket.on('enter', (user) => {
         users[socket.id] = user.nick;
         io.emit('listUsers', users);
     });
+
     socket.on('message', (user) => {
         io.emit('message', {
             'nick' : users[socket.id],
-            'message' : user.message
+            'message' : user.message,
+            'date' : formatDate(new Date())
         });
     })
+
     socket.on('disconnect', () => {
         delete users[socket.id];
         io.emit('listUsers', users);
+        io.emit('countUsersOnline', Object.keys(users).length);
+    });
+});
+
+app.get('/handlebar', (req, res) => {
+    res.render('handlebar.hbs', {
+        'HeadTitle' : 'Hbs',
+        'BodyTitle' : 'Page Hbs',
+        'BodyContent' : `User agent: ${req.get('user-agent')}`
     });
 });
 
 server.listen(port);
 
 console.log(`Server started at http://${hostname}:${port}/`);
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-//     socket.on('message', (data) => {
-//         console.log(data);
-//     });
-//     setInterval(() => {
-//         socket.emit('message', 'Hello from server!');
-//     }, 10000);
-// });
+
+
+function formatDate(date) {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+
+    if (day < 10) day = '0' + day;
+    if (month < 10) month = '0' + month;
+    if (hour < 10) hour = '0' + hour;
+    if (minutes < 10) minutes = '0' + minutes;
+
+    return `${hour}:${minutes} ${day}.${month}.${year}`;
+}
--- JS-05/www/client.js
+++ JS-05/www/client.js
@@ -3,19 +3,23 @@ const socket = io();
 let
     inputUserNick = document.getElementById('user-nick'),
     inputUserMessage = document.getElementById('user-message'),
-    btnUserEnter = document.getElementById('enter-nick'),
     btnSendMessage = document.getElementById('send-message'),
     divUsersList = document.getElementById('list-users'),
     divListMessagesUsers = document.getElementById('list-messages-users');
+    spanCountUsersOnline = document.getElementById('count-users-online');
 
-btnUserEnter.addEventListener('click', function () {
+inputUserNick.addEventListener('input', function () {
     let nick = inputUserNick.value.trim();
     socket.emit('enter', { 'nick' : nick });
 });
 
+inputUserMessage.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        sendMessage();
+    }
+});
 btnSendMessage.addEventListener('click', function () {
-    let message = inputUserMessage.value.trim();
-    socket.emit('message', { 'message' : message });
+    sendMessage();
 });
 
 socket.on('listUsers', (users) => {
@@ -29,6 +33,27 @@ socket.on('listUsers', (users) => {
 
 socket.on('message', (users) => {
     let divElem = document.createElement('div');
-    divElem.innerHTML = `${users.nick}: ${users.message}`;
-    divListMessagesUsers.appendChild(divElem);
+    let spanElemNick = document.createElement('span');
+    let spanElemDate = document.createElement('span');
+    spanElemDate.classList.add('date');
+    spanElemNick.innerHTML = `${users.nick}: `;
+    spanElemDate.innerHTML = users.date;
+    divElem.appendChild(spanElemNick);
+    divElem.innerHTML += `${users.message}`;
+    divElem.appendChild(spanElemDate);
+    divListMessagesUsers.insertBefore(divElem, divListMessagesUsers.firstChild);
 });
+
+socket.on('countUsersOnline', (count) => {
+    spanCountUsersOnline.innerHTML = count;
+});
+
+socket.on('user-nick', (nick) => {
+    inputUserNick.value = nick;
+});
+
+function sendMessage() {
+    let message = inputUserMessage.value.trim();
+    socket.emit('message', { 'message' : message });
+    inputUserMessage.value = '';
+}
