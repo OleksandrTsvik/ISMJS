2391689db291ccceeb7dd2ad13fd0be6d02d570d
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2021-05-02 21:37:25 +0300

    JS-04. Виправлено промальовування наосліп.

--- JS-04/js/index.js
+++ JS-04/js/index.js
@@ -43,11 +43,10 @@ let
     tempWidthPen,
     figureDrawing,
     x1, y1,
-    x2, y2,
-    paint = new Painting('c1');
+    paint = new Painting(canvas);
 
 canvas.addEventListener('mousedown', function(event) {
-    paint.changeMouseDown();
+    paint.setIsMouseDown(true);
 
     x1 = event.offsetX;
     y1 = event.offsetY;
@@ -59,44 +58,56 @@ canvas.addEventListener('mousedown', function(event) {
     }
 });
 
-canvas.addEventListener('mouseup', function(event) {
-    paint.changeMouseDown();
-
-    x2 = event.offsetX;
-    y2 = event.offsetY;
+canvas.addEventListener('mouseup', function() {
+    paint.setIsMouseDown(false);
 });
 
 canvas.addEventListener('mousemove', function(e) {
-    if (figureDrawing === 'Brush') {
-        paint.drawBrush(e.offsetX, e.offsetY, tempWidthPen, selectedColorPen.value);
-    } else if (figureDrawing === 'Line') {
-        paint.drawLine(x1, y1, x2, y2, tempWidthPen, selectedColorPen.value);
-    } else if (figureDrawing === 'RectangleStrokeAndFill' || figureDrawing === 'Rectangle') {
-        paint.drawRectangle(x1, y1, x2, y2, tempWidthPen, inputRectangleColorStroke.value, inputRectangleColorFill.value, true, true);
-    } else if (figureDrawing === 'RectangleFill') {
-        paint.drawRectangle(x1, y1, x2, y2, tempWidthPen, inputRectangleColorStroke.value, inputRectangleColorFill.value, false, true);
-    } else if (figureDrawing === 'RectangleStroke') {
-        paint.drawRectangle(x1, y1, x2, y2, tempWidthPen, inputRectangleColorStroke.value, inputRectangleColorFill.value);
-    } else if (figureDrawing === 'CircleStrokeAndFill' || figureDrawing === 'Circle') {
-        paint.drawCircle(x1, y1, x2, y2, tempWidthPen, inputCircleColorStroke.value, inputCircleColorFill.value, true, true);
-    } else if (figureDrawing === 'CircleFill') {
-        paint.drawCircle(x1, y1, x2, y2, tempWidthPen, inputCircleColorStroke.value, inputCircleColorFill.value, false, true);
-    } else if (figureDrawing === 'CircleStroke') {
-        paint.drawCircle(x1, y1, x2, y2, tempWidthPen, inputCircleColorStroke.value, inputCircleColorFill.value);
-    } else if (figureDrawing === 'EllipseStrokeAndFill' || figureDrawing === 'Ellipse') {
-        paint.drawEllipse(x1, y1, x2, y2, tempWidthPen, inputEllipseColorStroke.value, inputEllipseColorFill.value, true, true);
-    } else if (figureDrawing === 'EllipseFill') {
-        paint.drawEllipse(x1, y1, x2, y2, tempWidthPen, inputEllipseColorStroke.value, inputEllipseColorFill.value, false, true);
-    } else if (figureDrawing === 'EllipseStroke') {
-        paint.drawEllipse(x1, y1, x2, y2, tempWidthPen, inputEllipseColorStroke.value, inputEllipseColorFill.value);
-    } else if (figureDrawing === 'Eraser') {
-        paint.drawBrush(e.offsetX, e.offsetY, tempWidthPen, window.getComputedStyle(canvas).backgroundColor);
+    switch (figureDrawing) {
+        case 'Brush':
+            paint.redrawBrush(e.offsetX, e.offsetY, tempWidthPen, selectedColorPen.value);
+            break;
+        case 'Line':
+            paint.redrawLine(x1, y1, e.offsetX, e.offsetY, tempWidthPen, selectedColorPen.value);
+            break;
+        case 'Rectangle':
+        case 'RectangleStrokeAndFill':
+            paint.redrawRectangle(x1, y1, e.offsetX, e.offsetY, tempWidthPen, inputRectangleColorStroke.value, inputRectangleColorFill.value, true, true);
+            break;
+        case 'RectangleFill':
+            paint.redrawRectangle(x1, y1, e.offsetX, e.offsetY, tempWidthPen, inputRectangleColorStroke.value, inputRectangleColorFill.value, false, true);
+            break;
+        case 'RectangleStroke':
+            paint.redrawRectangle(x1, y1, e.offsetX, e.offsetY, tempWidthPen, inputRectangleColorStroke.value, inputRectangleColorFill.value);
+            break;
+        case 'Circle':
+        case 'CircleStrokeAndFill':
+            paint.redrawCircle(x1, y1, e.offsetX, e.offsetY, tempWidthPen, inputCircleColorStroke.value, inputCircleColorFill.value, true, true);
+            break;
+        case 'CircleFill':
+            paint.redrawCircle(x1, y1, e.offsetX, e.offsetY, tempWidthPen, inputCircleColorStroke.value, inputCircleColorFill.value, false, true);
+            break;
+        case 'CircleStroke':
+            paint.redrawCircle(x1, y1, e.offsetX, e.offsetY, tempWidthPen, inputCircleColorStroke.value, inputCircleColorFill.value);
+            break;
+        case 'Ellipse':
+        case 'EllipseStrokeAndFill':
+            paint.redrawEllipse(x1, y1, e.offsetX, e.offsetY, tempWidthPen, inputEllipseColorStroke.value, inputEllipseColorFill.value, true, true);
+            break;
+        case 'EllipseFill':
+            paint.redrawEllipse(x1, y1, e.offsetX, e.offsetY, tempWidthPen, inputEllipseColorStroke.value, inputEllipseColorFill.value, false, true);
+            break;
+        case 'EllipseStroke':
+            paint.redrawEllipse(x1, y1, e.offsetX, e.offsetY, tempWidthPen, inputEllipseColorStroke.value, inputEllipseColorFill.value);
+            break;
+        case 'Eraser':
+            paint.redrawBrush(e.offsetX, e.offsetY, tempWidthPen, window.getComputedStyle(canvas).backgroundColor);
+            break;
     }
 });
 
 canvas.addEventListener('mouseout', function() {
     paint.setIsMouseDown(false);
-    x1 = x2 = y1 = y2 = 0;
 });
 
 inputWidthPen.addEventListener('click', function() {
@@ -121,94 +132,57 @@ radioArrEllipse.forEach(radioBtn => radioBtn.addEventListener(('click'), functio
 }));
 
 radioBrushPen.addEventListener('click', function() {
-    changeDisplayStyleArrayTags(labelsStylePen, 'block');
-    changeDisplayStyleArrayTags(labelsStyleRectangle, 'none');
-    changeDisplayStyleArrayTags(labelsStyleCircle, 'none');
-    changeDisplayStyleArrayTags(labelsStyleEllipse, 'none');
+    changeDisplayStyleElemArrValue(labelsStylePen, 'block', labelsStyleRectangle, 'none', labelsStyleCircle, 'none', labelsStyleEllipse, 'none');
     spanColorPaint.innerHTML = 'Колір пензлика: ';
     spanWidthPaint.innerHTML = 'Товщина пензлика: ';
 });
 
 radioLinePen.addEventListener('click', function() {
-    changeDisplayStyleArrayTags(labelsStylePen, 'block');
-    changeDisplayStyleArrayTags(labelsStyleRectangle, 'none');
-    changeDisplayStyleArrayTags(labelsStyleCircle, 'none');
-    changeDisplayStyleArrayTags(labelsStyleEllipse, 'none');
+    changeDisplayStyleElemArrValue(labelsStylePen, 'block', labelsStyleRectangle, 'none', labelsStyleCircle, 'none', labelsStyleEllipse, 'none');
     spanColorPaint.innerHTML = 'Колір лінії: ';
     spanWidthPaint.innerHTML = 'Товщина лінії: ';
 });
 
 radioRectanglePen.addEventListener('click', function() {
     radioArrRectangle[2].checked = true;
-    changeDisplayStyleArrayTags(labelsStyleRectangle, 'block');
-    changeDisplayStyleArrayTags(labelsStylePen, 'none');
-    changeDisplayStyleArrayTags(labelsStyleCircle, 'none');
-    changeDisplayStyleArrayTags(labelsStyleEllipse, 'none');
+    changeDisplayStyleElemArrValue(labelsStylePen, 'none', labelsStyleRectangle, 'block', labelsStyleCircle, 'none', labelsStyleEllipse, 'none');
     spanWidthPaint.innerHTML = 'Ширина контуру: ';
 });
 
 radioCirclePen.addEventListener('click', function() {
     radioArrCircle[2].checked = true;
-    changeDisplayStyleArrayTags(labelsStyleCircle, 'block');
-    changeDisplayStyleArrayTags(labelsStylePen, 'none');
-    changeDisplayStyleArrayTags(labelsStyleRectangle, 'none');
-    changeDisplayStyleArrayTags(labelsStyleEllipse, 'none');
+    changeDisplayStyleElemArrValue(labelsStylePen, 'none', labelsStyleRectangle, 'none', labelsStyleCircle, 'block', labelsStyleEllipse, 'none');
     spanWidthPaint.innerHTML = 'Ширина контуру: ';
 });
 
 radioEllipsePen.addEventListener('click', function() {
     radioArrEllipse[2].checked = true;
-    changeDisplayStyleArrayTags(labelsStyleEllipse, 'block');
-    changeDisplayStyleArrayTags(labelsStylePen, 'none');
-    changeDisplayStyleArrayTags(labelsStyleRectangle, 'none');
-    changeDisplayStyleArrayTags(labelsStyleCircle, 'none');
+    changeDisplayStyleElemArrValue(labelsStylePen, 'none', labelsStyleRectangle, 'none', labelsStyleCircle, 'none', labelsStyleEllipse, 'block');
     spanWidthPaint.innerHTML = 'Ширина контуру: ';
 });
 
 radioClearPen.addEventListener('click', function() {
-    changeDisplayStyleArrayTags(labelsStylePen, 'none');
-    changeDisplayStyleArrayTags(labelsStyleRectangle, 'none');
-    changeDisplayStyleArrayTags(labelsStyleCircle, 'none');
-    changeDisplayStyleArrayTags(labelsStyleEllipse, 'none');
+    changeDisplayStyleElemArrValue(labelsStylePen, 'none', labelsStyleRectangle, 'none', labelsStyleCircle, 'none', labelsStyleEllipse, 'none');
     spanWidthPaint.innerHTML = 'Товщина гумки: ';
 });
 
-radioRectangleStroke.addEventListener('click', function() {
-    changeDisplayStyle(labelsStyleRectangle[4], 'block');
-    changeDisplayStyle(labelsStyleRectangle[3], 'none');
-});
-radioRectangleFill.addEventListener('click', function() {
-    changeDisplayStyle(labelsStyleRectangle[4], 'none');
-    changeDisplayStyle(labelsStyleRectangle[3], 'block');
-});
-radioRectangleStrokeAndFill.addEventListener('click', function() {
-    changeDisplayStyle(labelsStyleRectangle[4], 'block');
-    changeDisplayStyle(labelsStyleRectangle[3], 'block');
-});
-radioCircleStroke.addEventListener('click', function() {
-    changeDisplayStyle(labelsStyleCircle[3], 'none');
-    changeDisplayStyle(labelsStyleCircle[4], 'block');
-});
-radioCircleFill.addEventListener('click', function() {
-    changeDisplayStyle(labelsStyleCircle[3], 'block');
-    changeDisplayStyle(labelsStyleCircle[4], 'none');
-});
-radioCircleStrokeAndFill.addEventListener('click', function() {
-    changeDisplayStyle(labelsStyleCircle[3], 'block');
-    changeDisplayStyle(labelsStyleCircle[4], 'block');
-});
-radioEllipseStroke.addEventListener('click', function() {
-    changeDisplayStyle(labelsStyleEllipse[3], 'none');
-    changeDisplayStyle(labelsStyleEllipse[4], 'block');
-});
-radioEllipseFill.addEventListener('click', function() {
-    changeDisplayStyle(labelsStyleEllipse[3], 'block');
-    changeDisplayStyle(labelsStyleEllipse[4], 'none');
-});
-radioEllipseStrokeAndFill.addEventListener('click', function() {
-    changeDisplayStyle(labelsStyleEllipse[3], 'block');
-    changeDisplayStyle(labelsStyleEllipse[4], 'block');
-});
+radioRectangleStroke.addEventListener('click', () => changeDisplayStyleElemValue(labelsStyleRectangle[3], 'none', labelsStyleRectangle[4], 'block'));
+
+radioRectangleFill.addEventListener('click', () => changeDisplayStyleElemValue(labelsStyleRectangle[3], 'block', labelsStyleRectangle[4], 'none'));
+
+radioRectangleStrokeAndFill.addEventListener('click', () => changeDisplayStyleElemValue(labelsStyleRectangle[3], 'block', labelsStyleRectangle[4], 'block'));
+
+radioCircleStroke.addEventListener('click', () => changeDisplayStyleElemValue(labelsStyleCircle[3], 'none', labelsStyleCircle[4], 'block'));
+
+radioCircleFill.addEventListener('click', () => changeDisplayStyleElemValue(labelsStyleCircle[3], 'block', labelsStyleCircle[4], 'none'));
+
+radioCircleStrokeAndFill.addEventListener('click', () => changeDisplayStyleElemValue(labelsStyleCircle[3], 'block', labelsStyleCircle[4], 'block'));
+
+radioEllipseStroke.addEventListener('click', () => changeDisplayStyleElemValue(labelsStyleEllipse[3], 'none', labelsStyleEllipse[4], 'block'));
+
+radioEllipseFill.addEventListener('click', () => changeDisplayStyleElemValue(labelsStyleEllipse[3], 'block', labelsStyleEllipse[4], 'none'));
+
+radioEllipseStrokeAndFill.addEventListener('click', () => changeDisplayStyleElemValue(labelsStyleEllipse[3], 'block', labelsStyleEllipse[4], 'block'));
 
 btnClearCanvas.addEventListener('click', function() {
     paint.clear();
@@ -220,25 +194,23 @@ canvas.height = window.innerHeight * 0.81;
 radioArrPens[0].checked = true;
 figureDrawing = 'Brush';
 inputWidthPen.value = 10;
-changeDisplayStyleArrayTags(labelsStyleRectangle, 'none');
-changeDisplayStyleArrayTags(labelsStyleCircle, 'none');
-changeDisplayStyleArrayTags(labelsStyleEllipse, 'none');
+changeDisplayStyleElemArrValue(labelsStyleRectangle, 'none', labelsStyleCircle, 'none', labelsStyleEllipse, 'none');
 
-for (let i = 1; i <= 2000; ) {
+let objSteps = {
+    firstStep: 1,
+    15: 5,
+    100: 25,
+    500: 50,
+    1000: 100
+};
+let step = objSteps.firstStep;
+
+for (let i = 1; i <= 2000; i += step) {
     let elemOption = document.createElement('option');
     elemOption.innerHTML = i.toString();
     listWidthPen.append(elemOption);
-    if (i < 15) {
-        i++;
-    } else if (i >= 15 && i < 100) {
-        i += 5;
-    } else if (i >= 100 && i < 500) {
-        i += 25;
-    } else if (i >= 500 && i < 1000) {
-        i += 50;
-    } else if (i >= 1000 && i < 2000) {
-        i += 100;
-    } else break;
+    if (objSteps[i])
+        step = objSteps[i];
 }
 // ****************************************************
 
@@ -249,4 +221,16 @@ function changeDisplayStyle(idTag, value) {
 function changeDisplayStyleArrayTags(arrIdTags, value) {
     for (let i = 0; i < arrIdTags.length; i++)
         changeDisplayStyle(arrIdTags[i], value);
+}
+
+function changeDisplayStyleElemValue() {
+    for (let i = 0; i < arguments.length - 1; i += 2) {
+        changeDisplayStyle(arguments[i], arguments[i + 1]);
+    }
+}
+
+function changeDisplayStyleElemArrValue() {
+    for (let i = 0; i < arguments.length - 1; i += 2) {
+        changeDisplayStyleArrayTags(arguments[i], arguments[i + 1]);
+    }
 }
--- JS-04/js/libs/Painting.js
+++ JS-04/js/libs/Painting.js
@@ -4,8 +4,9 @@ export class Painting {
     _colorFill;
     _colorStroke;
     _isMouseDown;
-    constructor(idCanvas, colorFill = 'black', colorStroke = 'black') {
-        this._canvas = document.getElementById(idCanvas);
+    #drawingHistory = [{'painting': 'completed'}];
+    constructor(canvas, colorFill = 'black', colorStroke = 'black') {
+        this._canvas = canvas;
         this._ctx = this._canvas.getContext('2d');
         this._colorFill = colorFill;
         this._colorStroke = colorStroke;
@@ -31,7 +32,7 @@ export class Painting {
     }
 
     drawLine(x1, y1, x2, y2, radius, color = this._colorStroke) {
-        if (!this._isMouseDown) {
+        if (this._isMouseDown) {
             this.beginPath(radius, color, color);
             this._ctx.moveTo(x1, y1);
             this._ctx.lineTo(x2, y2);
@@ -40,7 +41,7 @@ export class Painting {
     }
 
     drawRectangle(x1, y1, x2, y2, radius, colorStroke = 'black', colorFill = 'black', isBorder = true, isFill = false) {
-        if (!this._isMouseDown) {
+        if (this._isMouseDown) {
             this.beginPath(radius, colorStroke, colorFill);
             this._ctx.rect(x1, y1, x2 - x1, y2 - y1);
             this.closePath(isBorder, isFill);
@@ -48,7 +49,7 @@ export class Painting {
     }
 
     drawCircle(x1, y1, x2, y2, radius, colorStroke = 'black', colorFill = 'black', isBorder = true, isFill = false) {
-        if (!this._isMouseDown) {
+        if (this._isMouseDown) {
             this.beginPath(radius, colorStroke, colorFill);
             this._ctx.arc(x1, y1,
                     Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5) / 2,
@@ -58,7 +59,7 @@ export class Painting {
     }
 
     drawEllipse(x1, y1, x2, y2, radius, colorStroke = 'black', colorFill = 'black', isBorder = true, isFill = false) {
-        if (!this._isMouseDown) {
+        if (this._isMouseDown) {
             this.beginPath(radius, colorStroke, colorFill);
             this._ctx.ellipse(x1, y1, Math.abs(x2 - x1), Math.abs(y2 - y1), 0, 0, 2 * Math.PI, false);
             this.closePath(isBorder, isFill);
@@ -79,7 +80,80 @@ export class Painting {
         this._ctx.closePath();
     }
 
+    redrawBrush(x, y, radius, colorFill) {
+        if (this._isMouseDown) {
+            this.#drawingHistory.push( {'draw': 'Brush', 'painting': 'completed', x, y, radius, colorFill} );
+            this.drawBrush(x, y, radius, colorFill);
+        } else if (this.#drawingHistory[this.#drawingHistory.length - 1].draw !== 'beginPath') {
+            this.#drawingHistory.push({'draw': 'beginPath', 'painting': 'completed'});
+        }
+    }
+
+    redrawLine(x1, y1, x2, y2, radius, color) {
+        this.#redrawShape('Line', {x1, y1, x2, y2, radius, color});
+    }
+
+    redrawRectangle(x1, y1, x2, y2, radius, colorStroke = 'black', colorFill, isBorder, isFill) {
+        this.#redrawShape('Rectangle', {x1, y1, x2, y2, radius, colorStroke, colorFill, isBorder, isFill});
+    }
+
+    redrawCircle(x1, y1, x2, y2, radius, colorStroke, colorFill, isBorder, isFill) {
+        this.#redrawShape('Circle', {x1, y1, x2, y2, radius, colorStroke, colorFill, isBorder, isFill});
+    }
+
+    redrawEllipse(x1, y1, x2, y2, radius, colorStroke = 'black', colorFill = 'black', isBorder = true, isFill = false) {
+        this.#redrawShape('Ellipse', {x1, y1, x2, y2, radius, colorStroke, colorFill, isBorder, isFill});
+    }
+
+    #redrawShape(shape, propShape) {
+        if (this._isMouseDown) {
+            if (this.#drawingHistory[this.#drawingHistory.length - 1].painting === 'completed')
+                this.#drawingHistory.push( {'draw': shape, 'painting': 'started', ...propShape} );
+            else
+                this.#drawingHistory[this.#drawingHistory.length - 1] = {'draw': shape, 'painting': 'started', ...propShape};
+
+            this.redraw();
+        } else {
+            this.#drawingHistory[this.#drawingHistory.length - 1].painting = 'completed';
+        }
+    }
+
+    redraw() {
+        this.clearCanvas();
+
+        for (let i = 0; i < this.#drawingHistory.length; i++) {
+            if (this.#drawingHistory[i].draw !== 'Brush')
+                this._ctx.beginPath();
+
+            switch (this.#drawingHistory[i].draw) {
+                case 'Brush':
+                    this.drawBrush(this.#drawingHistory[i].x, this.#drawingHistory[i].y, this.#drawingHistory[i].radius, this.#drawingHistory[i].colorFill);
+                    break;
+                case 'Line':
+                    this.drawLine(this.#drawingHistory[i].x1, this.#drawingHistory[i].y1, this.#drawingHistory[i].x2, this.#drawingHistory[i].y2, this.#drawingHistory[i].radius, this.#drawingHistory[i].color);
+                    break;
+                case 'Rectangle':
+                    this.drawRectangle(this.#drawingHistory[i].x1, this.#drawingHistory[i].y1, this.#drawingHistory[i].x2, this.#drawingHistory[i].y2, this.#drawingHistory[i].radius, this.#drawingHistory[i].colorStroke, this.#drawingHistory[i].colorFill, this.#drawingHistory[i].isBorder, this.#drawingHistory[i].isFill);
+                    break;
+                case 'Circle':
+                    this.drawCircle(this.#drawingHistory[i].x1, this.#drawingHistory[i].y1, this.#drawingHistory[i].x2, this.#drawingHistory[i].y2, this.#drawingHistory[i].radius, this.#drawingHistory[i].colorStroke, this.#drawingHistory[i].colorFill, this.#drawingHistory[i].isBorder, this.#drawingHistory[i].isFill);
+                    break;
+                case 'Ellipse':
+                    this.drawEllipse(this.#drawingHistory[i].x1, this.#drawingHistory[i].y1, this.#drawingHistory[i].x2, this.#drawingHistory[i].y2, this.#drawingHistory[i].radius, this.#drawingHistory[i].colorStroke, this.#drawingHistory[i].colorFill, this.#drawingHistory[i].isBorder, this.#drawingHistory[i].isFill);
+                    break;
+                case 'beginPath':
+                    this._ctx.beginPath();
+                    break;
+            }
+        }
+    }
+
     clear() {
+        this.#drawingHistory = [{'painting': 'completed'}];
+        this.clearCanvas();
+    }
+
+    clearCanvas() {
         this._ctx.clearRect(0, 0, this._canvas.offsetWidth, this._canvas.offsetHeight);
     }
 
@@ -118,6 +192,7 @@ export class Painting {
 
     setIsMouseDown(value) {
         this._isMouseDown = value;
+        this._ctx.beginPath();
     }
     getIsMouseDown() {
         return this._isMouseDown;
